fb320176d18f9f5808e5764782afb8a8f74c90eb
RudyPark3091 <grayblack313@gmail.com>
2020-09-08 20:46:24 +0900

    math3 cpp 3 probs
    1037
    11653
    5086

---
+++ cpp/11653.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 2; n > 1; ) {
+        if (n % i == 0) { n /= i; printf("%d\n", i); }
+        else { i++; }
+    }
+
+    return 0;
+}
