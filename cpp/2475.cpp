7accee7d80912fac187e62d237de24c0daca7371
RudyPark3091 <grayblack313@gmail.com>
2021-01-15 19:48:39 +0900

    2475.cpp

---
+++ cpp/2475.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[5];
+	int res = 0;
+	for (int i = 0; i < 5; i++)
+		cin >> arr[i];
+	for (int i = 0; i < 5; i++) {
+		res += arr[i] * arr[i];
+	}
+	cout << res % 10 << '\n';
+	return 0;
+}
