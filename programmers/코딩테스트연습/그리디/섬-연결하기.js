09d777acd5ff332e81753a635851ebd0295db911
rudy3091 <grayblack313@gmail.com>
2021-12-10 14:26:14 +0900

    fix 섬연결하기

--- programmers/코딩테스트연습/그리디/섬-연결하기.js
+++ programmers/코딩테스트연습/그리디/섬-연결하기.js
@@ -1,10 +1,19 @@
+// union-find 기법을 이용한 kruskal 알고리즘으로 풀이
 function solution(n, costs) {
+  // cost순으로 오름차순 정렬
   const arr = Array.from(costs).sort((a, b) => a[2] - b[2]);
-  const parent = Array.from(new Array(n), (_, i) => i);
+  // parent[i] === i번째 노드의 부모 노드 번호
+  const parent = Array(n).fill(0).map((_, i) => i);
+
+  // 현재 연결되어있는 부모 노드를 탐색
+  // 탐색과정에서 재귀적으로 자신의 부모를 갱신
   const findParent = x => {
     if (parent[x] !== x) parent[x] = findParent(parent[x]);
     return parent[x];
   }
+
+  // x, y 노드를 연결
+  // 번호가 낮은 노드를 부모로 설정
   const union = (x, y) => {
     const a = findParent(x);
     const b = findParent(y);
@@ -12,6 +21,9 @@ function solution(n, costs) {
     else parent[b] = a;
   }
 
+  // cost가 낮은 순으로 정렬된 edge들을 순회하며 노드를 연결
+  // edge의 양끝 노드의 부모가 같다면 이미 연결된 노드
+  // 부모가 다르다면 연결시켜줌
   let ans = 0;
   arr.forEach((item) => {
     const a = item[0];
